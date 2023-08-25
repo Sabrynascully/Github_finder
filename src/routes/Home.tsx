e6b7015ecc029a3b16a56c91cd4308d893741f55
Sabryna <97314825+Sabrynascully@users.noreply.github.com>
2023-08-25 00:19:13 +0000

    consumo da api

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,22 @@
 import Search from "../components/Search";
+import { UserProps } from "../types/user";
+import { useState } from "react";
+import axios from "axios";
 
-function Home() {
+const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  const loadUser = async (userName: string) => {
+    const res = await axios
+      .get(`https://api.github.com/users/${userName}`)
+      .then((data) => {
+        console.log(data.data);
+      });
+  };
   return (
     <div>
-      <Search />
+      <Search loadUser={loadUser} />
     </div>
   );
-}
+};
 export default Home;
