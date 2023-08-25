c8749658e74754aac24738a6d1bd6beee214860a
Sabryna <97314825+Sabrynascully@users.noreply.github.com>
2023-08-25 00:33:44 +0000

    exibindo nome do usuário

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,21 +1,28 @@
 import Search from "../components/Search";
 import { UserProps } from "../types/user";
 import { useState } from "react";
-import axios from "axios";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
 
   const loadUser = async (userName: string) => {
-    const res = await axios
-      .get(`https://api.github.com/users/${userName}`)
-      .then((data) => {
-        console.log(data.data);
-      });
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+
+    const data = await res.json();
+    const { avatar_url, login, location, followers, following } = data;
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+    setUser(userData);
   };
   return (
     <div>
       <Search loadUser={loadUser} />
+      {user && <p>{user.login}</p>}
     </div>
   );
 };
